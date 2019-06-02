02131f046f967cae9c7bff5b31220b5ab0911a03
rubina.karki <rubinakarki987@gmail.com>
2019-06-02 13:03:40 +0545

    error free commit

--- home/models.py
+++ home/models.py
@@ -7,8 +7,8 @@ class UrlInput(models.Model):
     shortenUrl = models.CharField(max_length =1000,null =True,blank =True)
     noOfHit = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(default= now,blank =True,null = True)
-    ip_address =  models.PositiveIntegerField(blank = True,null= True)
-
+    
     def __str__(self):
         return '%s' %(self.url)
 
+        
--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/',views.showHome,name ="home"),
-    path('<keyCode>',views.urlRedirect,name = "redirect"),
-    path('stat/<keyCode>',views.info,name = "info")
+    path('<keyCode>',views.urlRedirect,name ="redirect"),
+    path('stat/<keyCode>',views.info,name ="info")
 ]
